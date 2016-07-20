dd54b62aaaaa72ebf8a5cecb3633f613fc9e5f18
Thomas Desvenain <thomas.desvenain@gmail.com>
2016-07-20 15:24:32 +0200

    searchable_text is str not unicode

--- review/rfsepolicy/index.py
+++ review/rfsepolicy/index.py
@@ -6,6 +6,7 @@ def searchable(obj):
     searchable_text = obj.SearchableText()
     if obj.portal_type == 'article':
         if obj.auteur:
-            searchable_text += u" " + obj.auteur
+            searchable_text += " "
+            searchable_text += obj.auteur.encode('utf-8')
 
     return searchable_text
